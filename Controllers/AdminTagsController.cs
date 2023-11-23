0be88ff80a9c92fc3658eb20453c005a2a1cc040
Highbrook <MilivojevicDjole@yahoo.com>
2023-11-23 19:13:45 +0100

    Update

---
+++ Controllers/AdminTagsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers
+{
+    public class AdminTagsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult SubmitTag()
+        {
+            return View();
+        }
+    }
+}
