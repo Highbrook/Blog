55f80c77f9288b1b628394b3a9780b9f3c94d8e5
Highbrook <MilivojevicDjole@yahoo.com>
2023-11-23 21:58:02 +0100

    Update
    Added submitting to DB

--- Controllers/AdminTagsController.cs
+++ Controllers/AdminTagsController.cs
@@ -1,18 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Blog.Data;
+using Blog.Models.Domain;
+using Blog.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.Controllers
 {
     public class AdminTagsController : Controller
     {
+        private BlogDbContext _blogDbContext;
+        public AdminTagsController(BlogDbContext blogDbContext)
+        {
+            _blogDbContext = blogDbContext;
+        }
+
         [HttpGet]
         public IActionResult Add()
         {
             return View();
         }
         [HttpPost]
-        public IActionResult SubmitTag()
+        public IActionResult Add(AddTagRequest addTagRequest)
         {
-            return View();
+            // Mapping AddTagRequest to Tag domain model
+            var tag = new Tag
+            {
+                Name = addTagRequest.Name,
+                DisplayName = addTagRequest.DisplayName
+            };
+
+            _blogDbContext.Tags.Add(tag);
+            _blogDbContext.SaveChanges();
+
+            return View("Add");
         }
     }
 }
